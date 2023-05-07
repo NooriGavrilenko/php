aa44401a2677325d617b0022118892d1af4eac0d
Нурия <gladness.90@mail.ru>
2023-05-07 12:39:45 +0400

    init

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -2,6 +2,18 @@
 window.onload = function()
 {
     const initPerson = personGenerator.getPerson();
+
+    document.getElementById('surnameOutput').innerText = initPerson.familiya;
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('genderOutput').innerText = initPerson.gender;
+    document.getElementById('birthYearOutput').innerText = initPerson.dataRojdeniya;
 };
 
+
+
+document.getElementById('buttonClear').addEventListener('click', function () {
+    document.getElementById('surnameOutput').innerText = "Генерация фамилии";
+    document.getElementById('firstNameOutput').innerText = "Генерация имени";
+    document.getElementById('genderOutput').innerText = "Генерация пола";
+    document.getElementById('birthYearOutput').innerText = "Генерация даты рождения";
+});
--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -36,6 +36,25 @@ const personGenerator = {
         }
     }`,
 
+    
+    
+
+    firstNameFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Ольга",
+            "id_2": "Анастасия",
+            "id_3": "Ирина",
+            "id_4": "Арина",
+            "id_5": "Мария",
+            "id_6": "Гуля",
+            "id_7": "Ксения",
+            "id_8": "Алефтина",
+            "id_9": "Евгения",
+            "id_10": "Валентина"
+        }
+    }`,
+
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
 
@@ -47,10 +66,13 @@ const personGenerator = {
         return obj.list[prop];
     },
 
-    randomFirstName: function() {
-
-        return this.randomValue(this.firstNameMaleJson);
-
+    randomFirstName: function(pol) {
+        if (pol == 0) {
+            return this.randomValue(this.firstNameFemaleJson);
+        }
+        else {
+            return this.randomValue(this.firstNameMaleJson);
+        }
     },
 
 
@@ -60,11 +82,56 @@ const personGenerator = {
 
     },
 
+    randomGender: function() {
+        const pol = this.randomIntNumber(1, 0);
+        return pol;
+    },
 
     getPerson: function () {
+
+        monthArray = {                
+            1: "Январь",
+            2: "Февраль",
+            3: "Март",
+            4: "Апрель",
+            5: "Май",
+            6: "Июнь",
+            7: "Июль",
+            8: "Август",
+            9: "Сентябрь",
+            10: "Октябрь",
+            11: "Ноябрь",
+            12: "Декабрь"
+        };
+
+
+        const pol = this.randomGender();
+        var gender = (pol == 0)? this.GENDER_FEMALE : this.GENDER_MALE;
+
+        var familiya = this.randomSurname();
+        var oconcanie =  (pol == 0)? 'а' : '';
+
+        var godRojdeniya = this.randomIntNumber(1962, 2010).toString();
+        var month = this.randomIntNumber(1, 12);
+
+        
+        var strMonth = monthArray[month];
+        
+        var posDayMonth = 31;
+        if (month == 2) {
+            posDayMonth = 28;
+        }
+        else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            posDayMonth = 30;
+        }
+        var denRojdeniya = this.randomIntNumber(1, posDayMonth).toString();
+        var dataRojdeniya = (denRojdeniya + " " + strMonth + " " + godRojdeniya);
+
         this.person = {};
-        // this.person.gender = this.randomGender();
-        this.person.firstName = this.randomFirstName();
+        this.person.familiya = (familiya + oconcanie);
+        this.person.firstName = this.randomFirstName(pol);
+        this.person.gender = gender;
+        this.person.dataRojdeniya = dataRojdeniya;
         return this.person;
     }
 };
